122f7254778631ce68fa44b7d3c8a8b49ef29cc3
吉田竜一郎 <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-12-04 15:09:02 +0900

    GASのURL修正、シェルスクリプトの修正

--- UnityBuildShell/team-b-build.sh
+++ UnityBuildShell/team-b-build.sh
@@ -2,9 +2,9 @@
 UNITY_VERSION=2022.3.48f1
 UNITY_EDITOR_PATH=/Applications/Unity/Hub/Editor/
 PROJECT_PATH=/Users/vantan/Desktop/TeamB
-EXPORT_PATH=/Users/vantan/Library/CloudStorage/GoogleDrive-vtnstorage.2023@gmail.com/その他のパソコン/マイ コンピュータ/Artifacts/Team2024
+EXPORT_PATH="/Users/vantan/Library/CloudStorage/GoogleDrive-vtnstorage.2023@gmail.com/その他のパソコン/マイ コンピュータ/Artifacts/Team2024"
 LOG_FILE_PATH=$PROJECT_PATH/log/TeamB.log
-GAS_URL=https://script.google.com/macros/s/AKfycbwFE95eSPBTax07d3mrhXX1FKK8uWJnbdqLZM_8toC07dVhlV23yaAWeb8NCFeVa56Q/exec?folder=U2FsdGVkX1/ierC9UJRzXur8wFbb1HDCi8NrAerh06MO3ubM3FZ+HrmCKnuaCxui5VxGzucci/kr0FxqOdXzZQ==&team=B
+GAS_URL="https://script.google.com/macros/s/AKfycbz8goqh4NBZpD6v-mp4WCSoEZlPHuhOC2Yz5gq884ykcD0eP7lfBhVapedfLMUhzzAqjw/exec?folder=U2FsdGVkX1/ierC9UJRzXur8wFbb1HDCi8NrAerh06MO3ubM3FZ+HrmCKnuaCxui5VxGzucci/kr0FxqOdXzZQ==&team=B"
 
 # 出力フォルダを処理のたびに削除する
 if [ -e "$PROJECT_PATH/Build" ]; then
@@ -21,8 +21,7 @@ if [ $? -eq 1 ]; then
 fi
 
 # Unityビルドコマンドを実行する
-"$UNITY_EDITOR_PATH$UNITY_VERSION/Editor/Unity.exe" -batchmode -quit -projectPath "$PROJECT_PATH" -executeMethod "BuildCommand.Build" -logfile "$LOG_FILE_PATH" -platform "Mac" -devmode true -outputPath "$PROJECT_PATH/Build"
-
+"$UNITY_EDITOR_PATH$UNITY_VERSION/Unity.app/Contents/MacOS/Unity" -batchmode -quit -projectPath "$PROJECT_PATH" -executeMethod "BuildCommand.Build" -logfile "$LOG_FILE_PATH" -platform "Mac" -devmode true -outputPath "$PROJECT_PATH/Build"
 if [ $? -eq 1 ]; then
     cat "./log/TeamB.log"
     exit 1
@@ -35,8 +34,6 @@ if [ $? -eq 1 ]; then
     exit 1
 fi
 
-mv "$PROJECT_PATH\TeamB.zip" "$EXPORT_PATH"
+mv "$PROJECT_PATH/TeamB.zip" "$EXPORT_PATH"
 
 curl -f "$GAS_URL"
-
-pause
